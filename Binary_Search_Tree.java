a53e30300f37a96902c2ef131253ba62d471e56b
Manish kumar prasad <manishkumar9798566@gmail.com>
2026-08-20 00:56:43 +0530

    Add files via upload

---
+++ Binary_Search_Tree.java
@@ -0,0 +1,85 @@
+class Binary_Search_Tree {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node root;
+
+    
+    Node insert(Node root, int data) {
+        if (root == null)
+            return new Node(data);
+
+        if (data < root.data)
+            root.left = insert(root.left, data);
+        else
+            root.right = insert(root.right, data);
+
+        return root;
+    }
+
+    
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    void preorder(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    void postorder(Node root) {
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    
+    boolean search(Node root, int key) {
+        if (root == null)
+            return false;
+
+        if (root.data == key)
+            return true;
+
+        if (key < root.data)
+            return search(root.left, key);
+        else
+            return search(root.right, key);
+    }
+
+    public static void main(String[] args) {
+        Binary_Search_Tree tree = new Binary_Search_Tree();
+
+        int[] a = {50, 30, 70, 20, 40, 60, 80, 10};
+
+        for (int x : a)
+            tree.root = tree.insert(tree.root, x);
+
+        System.out.print("Inorder: ");
+        tree.inorder(tree.root);
+
+        System.out.print("\nPreorder: ");
+        tree.preorder(tree.root);
+
+        System.out.print("\nPostorder: ");
+        tree.postorder(tree.root);
+
+        System.out.println("\n\nSearch 40: " + tree.search(tree.root, 40));
+        System.out.println("Search 100: " + tree.search(tree.root, 100));
+    }
+}
